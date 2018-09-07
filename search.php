b2790166cfcea85cbb66dcb97fd0f3dc93787063
budi1200 <alenab22@gmail.com>
2018-09-07 21:33:08 +0200

    add search

---
+++ search.php
@@ -0,0 +1,18 @@
+<?php
+    require("./connect.php");
+    require("./components/header.php");
+
+    if(isset($_GET['s'])){
+        $string = strtolower($_GET['s']);
+
+        $sql = $conn->prepare("SELECT * FROM games WHERE LOWER(title) LIKE ?");
+        $sql->execute(array("%$string%"));
+        $result = $sql->fetchAll();
+
+        foreach($result as $row){
+            echo $row['title'];
+            echo '<a href="./game/gameDetails.php?game_id=' . $row['id'] . '">More</a>';
+        }
+    }
+
+?>
