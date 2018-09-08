d950a595696e0708afb58ae2f64859777c43e8f1
budi1200 <alenab22@gmail.com>
2018-09-08 23:57:58 +0200

    add image upload/edit

--- Game/addGame.php
+++ Game/addGame.php
@@ -12,17 +12,19 @@
     <title>Add Game</title>
 </head>
 <body>
-    <form action="" method="post">
+    <form action="" method="post" enctype="multipart/form-data">
         <label>Title:</label><input type="text" name="title"/>
         <label>Description:</label><input type="text" name="about"/>
         <label>Genre</label><input type="text" name="genre"/>
         <label>Release year:</label><input type="text" name="release_year"/> <!-- TODO: Modify to accept only year -->
         <label>Developer:</label><input type="text" name="developer"/>
         <label>Website:</label><input type="text" name="website_url"/>
+        <label>Poster image:</label><input type="file" name="img-file"/>
         <button type="submit" name="submit">Add Game</button>
     </form>
 
     <?php
+        // handle add game button press
         if(isset($_POST['submit'])){
             $title = $_POST['title'];
             $about = $_POST['about'];
@@ -31,17 +33,34 @@
             $developer = $_POST['developer'];
             $website = $_POST['website_url'];
             $user_id = $_SESSION['user_id'];
-            $image_id = 0;
 
-            $sql = $conn->prepare("INSERT INTO games(user_id, image_id, title, about, genre, release_year, developer, website_url) VALUES (?,?,?,?,?,?,?,?)");
-            try{
-                $sql->execute(array($user_id, $image_id, $title, $about, $genre, $release_year, $developer, $website));
-                
-                $sql2 = $conn->prepare("SELECT id FROM games WHERE user_id=? AND title=?");
-                $sql2->execute(array($user_id, $title));
-                $result = $sql2->fetch();
-                header("Location: ./gameDetails.php?game_id=".$result['id']);
-            }catch(PDOException $err){
+
+            // Handle image upload
+            if($_FILES['img-file']['error'] > 0){ 
+                echo 'Error during uploading, try again'; 
+            }
+            
+            $extsAllowed = array( 'jpg', 'jpeg', 'png', 'gif' );
+            $extUpload = strtolower( substr( strrchr($_FILES['img-file']['name'], '.') ,1) ) ;
+            $name = "/game-library/img/{$_FILES['img-file']['name']}";
+            $result3 = move_uploaded_file($_FILES['img-file']['tmp_name'], "../img/{$_FILES['img-file']['name']}");
+            $sql_img = $conn->prepare("INSERT INTO images(url) VALUES (?)");
+            $sql_img->execute(array($name));
+            $img_id = $conn->lastInsertId(); 
+
+            // on successful upload insert game into database
+            if($result3){                
+                try{
+                    $sql = $conn->prepare("INSERT INTO games(user_id, image_id, title, about, genre, release_year, developer, website_url) VALUES (?,?,?,?,?,?,?,?)");
+                    $sql->execute(array($user_id, $img_id, $title, $about, $genre, $release_year, $developer, $website));
+                    $sql2 = $conn->prepare("SELECT id FROM games WHERE user_id=? AND title=?");
+                    $sql2->execute(array($user_id, $title));
+                    $result = $sql2->fetch();
+                    header("Location: ./gameDetails.php?game_id=".$result['id']);
+                }catch(PDOException $err){
+                    echo "Error";
+                }
+            }else{
                 echo "Error";
             }
         }
--- Game/gameDetails.php
+++ Game/gameDetails.php
@@ -6,7 +6,7 @@
     if(isset($_GET['game_id'])){
         $game_id = $_GET['game_id'];
         
-        $sql = $conn->prepare("SELECT g.*, i.url FROM games g INNER JOIN images i ON g.id=i.id WHERE g.id=?");
+        $sql = $conn->prepare("SELECT g.*, i.url FROM games g INNER JOIN images i ON g.image_id=i.id WHERE g.id=?");
         $sql->execute(array($game_id));
         $result = $sql->fetch();
         if(!$result){
@@ -62,25 +62,30 @@
     <?php echo "<title>Game Library | " . $result['title'] . "</title>"; ?>
 </head>
 <body>
-    <?php
+    <?php  
+        // include header
         require("../components/header.php");
+
         echo $result['title'] . "<br/>";
         echo $result['about'] . "<br/>";
         echo $result['genre'] . "<br/>";
         echo $result['release_year'] . "<br/>";
         echo $result['developer'] . "<br/>";
         echo $result['website_url'] . "<br/>";
-        echo $result['url'] . "<br/>";
+        echo '<img src="' . $result['url'] . '"/><br/>';
         echo "Average user rating: " . $result_avgRating['avgRating'] . "<br/>";
+        // if logged in show rate button
         if(isset($_SESSION['user_id'])){
             echo '<form action="" method="post"><label>Rating: </label><input type="number" max=5 min=1 value=3 name="rating"/><br/><button type="submit">'.$rateString.'</button></form>';
         }
+
+        // if owner show edit and delete buttons
         if(isset($_SESSION['user_id']) && $_SESSION['user_id'] == $result['user_id']){
             echo '<form action="editGame.php" method="post"> <input type="hidden" name="game_id" value="' . $result['id'] . '"/> <button type="submit">Edit Game</button></form>';
             echo '<form action="removeGame.php" method="post"> <input type="hidden" name="id" value="' . $result['id'] . '"/> <button type="submit">Delete Game</button></form>';
         }
 
-        // comment section
+        // **COMMENT SECTION**
         echo '<form action="" method="post">';
             echo '<textarea name="text"></textarea>';
             echo '<button type="submit">Comment</button>';
