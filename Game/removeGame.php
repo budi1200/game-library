44a4b0467ab7cd8a99b24e71a4a05a17dd4f7668
budi1200 <alenab22@gmail.com>
2018-09-09 09:48:20 +0200

    remove game comments when deleting game

--- Game/removeGame.php
+++ Game/removeGame.php
@@ -3,9 +3,11 @@
     require("../requireLogin.php");
 
     // TODO: Add owner verification
-    $sql = $conn->prepare("DELETE FROM games WHERE id=? AND user_id=?");
     try{
+        $sql = $conn->prepare("DELETE FROM games WHERE id=? AND user_id=?");
         $sql->execute(array($_POST['id'], $_SESSION['user_id']));
+        $sql_comments = $conn->prepare("DELETE FROM comments WHERE game_id=?");
+        $sql_comments->execute(array($_POST['id']));
         header("Location: /game-library/index.php");
     }catch(PDOException $err){
         echo "Error";
